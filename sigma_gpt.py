f267bd98c6895a377bba1e5f9ec30fb8a438ed92
xMohamed <54814433+MazX0p@users.noreply.github.com>
2023-03-29 01:25:33 +0300

    Update sigma_gpt.py

--- sigma_gpt.py
+++ sigma_gpt.py
@@ -2,8 +2,9 @@ import requests
 import json
 import os
 import yaml
+import xml.etree.ElementTree as ET
 from colorama import init, Fore, Style
-from Evtx import Evtx
+from evtx import PyEvtxParser
 
 # Load the ChatGPT API key from an environment variable
 api_key = os.environ.get('CHATGPT_API_KEY')
@@ -49,25 +50,35 @@ for path in sigma_rules_paths:
                         print(f'Error: No rules found in file "{os.path.join(path, filename)}"')
 
 # Process each event in the EVTX file and compare against the Sigma rules
-with Evtx(evtx_file_path) as evtx:
-    for record in evtx.records():
-        event = json.loads(record.json())
+parser = PyEvtxParser(evtx_file_path)
+for record in parser.records():
+    try:
+        event_xml = ET.fromstring(record['data'])
+        event_data = event_xml.find('.//EventData')
+        if event_data is not None:
+            event_dict = {data.attrib['Name']: (data.text if data.text is not None else '') for data in event_data}
+        else:
+            print('Error: Unable to find EventData in record:', record['data'])
+            continue
+    except ET.ParseError:
+        print('Error parsing XML data for record:', record['data'])
+        continue
 
-        for rule in sigma_rules:
-            try:
-                # Get the GPT-3.5 response for the Sigma rule and the EVTX event
-                response = requests.post('https://api.openai.com/v1/engines/davinci-codex/completions', headers={'Authorization': f'Bearer {api_key}'}, json={
-                    'prompt': f'{rule}\nEvent: {event["Event"]["EventData"]}\n',
-                    'max_tokens': 1024,
-                    'n': 1,
-                    'stop': '.'
-                }).json()
+    for rule in sigma_rules:
+        try:
+            # Get the GPT-3.5 response for the Sigma rule and the EVTX event
+            response = requests.post('https://api.openai.com/v1/engines/davinci-codex/completions', headers={'Authorization': f'Bearer {api_key}'}, json={
+                'prompt': f'{rule}\nEvent: {event["Event"]["EventData"]}\n',
+                'max_tokens': 1024,
+                'n': 1,
+                'stop': '.'
+            }).json()
 
-                # Check if the response contains any redacted text (indicating a potential security threat)
-                if '[REDACTED]' in response['choices'][0]['text']:
-                    # Print the Sigma rule and the event data, with relevant text highlighted in red
-                    print(Fore.RED + 'Rule: ' + str(rule))
-                    print(Fore.RED + 'Event: ' + json.dumps(event["Event"]["EventData"], indent=4))
-                    print(Fore.RESET)
-            except Exception as e:
-                print(f'Error processing Sigma rule: {e}')
+            # Check if the response contains any redacted text (indicating a potential security threat)
+            if '[REDACTED]' in response['choices'][0]['text']:
+                # Print the Sigma rule and the event data, with relevant text highlighted in red
+                print(Fore.RED + 'Rule: ' + str(rule))
+                print(Fore.RED + 'Event: ' + json.dumps(event["Event"]["EventData"], indent=4))
+                print(Fore.RESET)
+        except Exception as e:
+            print(f'Error processing Sigma rule: {e}')
